1a2108f125b6ce5769323366d8e1595e691bfbe2
Deihl_Reyes <78899978+DeihlReyes@users.noreply.github.com>
2026-03-06 11:02:14 +0800

    feat(auth): add user authentication models and routes through better-auth

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,6 +6,13 @@ import { PrismaClient } from "../generated/prisma/client";
 const connectionString = `${process.env.DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
 
-export { prisma };
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
